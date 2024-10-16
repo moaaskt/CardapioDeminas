98e7befc020e606ab2f1e0a577c9b16d882af328
moaaskt <moacirmayer@hotmail.com>
2024-10-16 02:56:10 -0300

    att

--- js/page.js
+++
@@ -1,7 +0,0 @@
-// script.js
-const nav = document.querySelector('nav');
-const navLinks = document.querySelector('.nav-links');
-
-document.querySelector('header').addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
